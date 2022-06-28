045c7441540f205aa4d3b610dab35d20b1c9dba5
tr <1289380467@qq.com>
2022-06-28 08:19:02 +0800

    添加客户端网络编程模块

---
+++ RemoteCtrl/RemoteClient/ClientSocket.h
@@ -0,0 +1,305 @@
+#pragma once
+#include "pch.h"
+#include "framework.h"
+#include <string>
+#pragma pack(push)
+#pragma pack(1)
+
+class CPacket
+{
+public:
+	WORD sHead;//包头。固定位 FEFF
+	DWORD nLength;//包长度（从控制命令到和校验结束）
+	WORD sCmd;//控制命令
+	std::string strData;//包数据
+	WORD sSum;//和校验
+	std::string strOut;//整个包的信息
+	CPacket() {}
+	CPacket(WORD nCmd, const BYTE* pData, size_t nSize)
+	{
+		sHead = 0xFEFF;
+		nLength = nSize + 4;
+		sCmd = nCmd;
+		if (nSize > 0)
+		{
+			strData.resize(nSize);
+			memcpy((void*)strData.c_str(), pData, nSize);
+		}
+		else
+		{
+			strData.clear();
+		}
+		sSum = 0;
+		for (size_t j = 0; j < strData.size(); j++)
+		{
+			sSum += BYTE(strData[j]) & 0xFF;
+		}
+	}
+	CPacket(const CPacket& mpack)
+	{
+		sHead = mpack.sHead;
+		nLength = mpack.nLength;
+		sCmd = mpack.sCmd;
+		strData = mpack.strData;
+		sSum = mpack.sSum;
+	}
+	CPacket(const BYTE* pData, size_t& nSize)
+	{
+		size_t i = 0;
+		for (; i < nSize; i++)//可能有问题
+		{
+			if (*(WORD*)(pData + i) == 0xFEFF)
+			{
+				sHead = *(WORD*)(pData + i);
+				i += 2;
+				break;
+			}
+		}
+		if (i + 4 + 2 + 2 > nSize)//包数据可能不全，或者包头未能全部接收到
+		{
+			nSize = 0;
+			return;
+		}
+		nLength = *(DWORD*)(pData + i); i += 4;
+		if (nLength + i > nSize)//包未完全接收到，就返回，解析失败
+		{
+			nSize = 0;
+			return;
+		}
+		sCmd = *(WORD*)(pData + i); i += 2;
+		if (nLength > 4)
+		{
+			strData.resize(nLength - 2 - 2);
+			memcpy((void*)strData.c_str(), pData + i, nLength - 4);
+			i += nLength - 4;
+		}
+		sSum = *(WORD*)(pData + i); i += 2;
+		WORD sum = 0;
+		for (size_t j = 0; j < strData.size(); j++)
+		{
+			sum += BYTE(strData[j]) & 0xFF;
+		}
+		if (sum == sSum)
+		{
+			nSize = i;//head 2,length 4,cmd+data+sum nLength
+			return;
+		}
+		nSize = 0;
+	}
+	~CPacket() {}
+	CPacket& operator =(const CPacket& mpack)
+	{
+		if (this != &mpack)
+		{
+			sHead = mpack.sHead;
+			nLength = mpack.nLength;
+			sCmd = mpack.sCmd;
+			strData = mpack.strData;
+			sSum = mpack.sSum;
+		}
+		return *this;
+	}
+	int Size()
+	{
+		return nLength + 2 + 4;
+	}
+	const char* Data()
+	{
+		strOut.resize(nLength + 6);
+		BYTE* pData = (BYTE*)strOut.c_str();
+		*(WORD*)pData = sHead; pData += 2;
+		*(DWORD*)pData = nLength; pData += 4;
+		*(WORD*)pData = sCmd; pData += 2;
+		memcpy(pData, strData.c_str(), strData.size()); pData += strData.size();
+		*(WORD*)pData = sSum;
+		return strOut.c_str();
+	}
+};
+#pragma pack(pop)
+
+typedef struct MouseEvent
+{
+	MouseEvent()
+	{
+		nAction = 0;
+		nButton = -1;
+		ptXY.x = 0;
+		ptXY.y = 0;
+	}
+	WORD nAction;//单击，双击，移动
+	WORD nButton;//左键，右键，中键
+	POINT ptXY;//坐标
+}MOUSEEV, * PMOUSSEV;
+
+
+std::string GetErrorInfo(int wsaErrCode)
+{
+	std::string ret;
+	LPVOID lpMsgBuf = NULL;
+	FormatMessage(
+		FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER,
+		NULL,
+		wsaErrCode,
+	MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPSTR)&lpMsgBuf, 0, NULL
+		);
+	ret = (char*)lpMsgBuf;
+	LocalFree(lpMsgBuf);
+	return ret;
+}
+
+class CClientSocket
+{
+public:
+	static CClientSocket* getInstance()
+	{
+		if (m_instance == NULL)//静态函数没有this指针，无法访问成员变量
+		{
+			m_instance = new CClientSocket();
+		}
+		return m_instance;
+	}
+	bool InitSocket(const std::string& strIPAddress)
+	{
+
+		//校验
+		if (m_sock == -1)
+		{
+			return false;
+		}
+		sockaddr_in serv_adr;
+		memset(&serv_adr, 0, sizeof(serv_adr));
+		serv_adr.sin_family = AF_INET;
+		serv_adr.sin_addr.s_addr = inet_addr(strIPAddress.c_str());
+		serv_adr.sin_port = htons(9527);
+		if (serv_adr.sin_addr.s_addr == INADDR_NONE)
+		{
+			AfxMessageBox("指定的IP地址不存在");
+			return false;
+		}
+		int ret=connect(m_sock, (sockaddr*)&serv_adr, sizeof(serv_adr));
+		if (ret == -1)
+		{
+			AfxMessageBox("连接失败");
+			TRACE("连接失败，%d %s\r\n", WSAGetLastError(), GetErrorInfo(WSAGetLastError()).c_str());
+		}
+		return true;
+	}
+
+#define BUFFER_SIZE 4096
+	int DealCommand()
+	{
+		if (m_sock == -1)
+		{
+			return -1;
+		}
+		//char buffer[1024] = "";
+		char* buffer = new char[BUFFER_SIZE];//接收包的缓冲区
+		memset(buffer, 0, BUFFER_SIZE);
+		size_t index = 0;
+		while (true)
+		{
+			size_t len = recv(m_sock, buffer + index, BUFFER_SIZE - index, 0);
+			if (len <= 0)
+			{
+				return -1;
+			}
+			index += len;
+			len = index;
+			m_packet = CPacket((BYTE*)buffer, len);
+			if (len > 0)
+			{
+				memmove(buffer, buffer + len, BUFFER_SIZE - len);
+				index -= len;
+				return m_packet.sCmd;
+			}
+		}
+		return -1;
+	}
+	bool Send(const char* pData, int nsize)
+	{
+		if (m_sock == -1)
+		{
+			return false;
+		}
+		return send(m_sock, pData, nsize, 0) > 0;
+	}
+	bool Send(CPacket& pack)
+	{
+		if (m_sock == -1)return false;
+		return send(m_sock, pack.Data(), pack.Size(), 0) > 0;
+	}
+	bool GetFilePath(std::string& strPath)
+	{
+		if ((m_packet.sCmd == 2) || (m_packet.sCmd == 3) || (m_packet.sCmd == 4))
+		{
+			strPath = m_packet.strData;
+			return true;
+		}
+		return false;
+	}
+	bool GetMouseEvent(MOUSEEV& mouse)
+	{
+		if (m_packet.sCmd == 5)
+		{
+			memcpy(&mouse, m_packet.strData.c_str(), sizeof(MOUSEEV));
+			return true;
+		}
+		return false;
+	}
+private:
+	SOCKET m_sock;
+	CPacket m_packet;
+	CClientSocket() {
+
+		if (InitSockEnv() == FALSE)
+		{
+			MessageBox(NULL, _T("无法初始化套接字环境，请检测网络设置！"), _T("初始化错误！"), MB_OK | MB_ICONERROR);
+			exit(0);
+		}
+		m_sock = socket(PF_INET, SOCK_STREAM, 0);
+	}
+	CClientSocket& operator=(const CClientSocket& ss) {}
+	CClientSocket(const CClientSocket& ss)
+	{
+		m_sock = ss.m_sock;
+	}
+	BOOL InitSockEnv()
+	{
+		WSADATA data;
+		if (WSAStartup(MAKEWORD(1, 1), &data) != 0)//返回值处理
+		{
+			return FALSE;
+		}
+		return TRUE;
+	}
+	~CClientSocket() {
+		closesocket(m_sock);
+		WSACleanup();
+	}
+	static void releaseInstance()
+	{
+		if (m_instance != NULL)
+		{
+			CClientSocket* tmp = m_instance;
+			m_instance = NULL;
+			delete tmp;
+		}
+	}
+	static CClientSocket* m_instance;
+	class CHelper
+	{
+	public:
+		CHelper()
+		{
+			CClientSocket::getInstance();
+		}
+		~CHelper()
+		{
+			CClientSocket::releaseInstance();
+		}
+	};
+	static CHelper m_helper;
+};
+
+extern CClientSocket server;
